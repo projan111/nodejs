c86cb44e9afa3ff435b2f5416ceafe7a15a514a1
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-20 21:11:29 +0545

    add read file

---
+++ file.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+//Sync
+// fs.writeFileSync("./text.txt", "Hello, I am from the nodejs.");
+
+//Async
+// fs.writeFile("./text.txt", "Hello, I am from the nodejs | Async.", (err) => {});
+
+// const result = fs.readFileSync("./contact.txt", "utf-8")
+// console.log(result)
+
+fs.readFile("./contact.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log("File Error", err);
+  } else {
+    console.log(result);
+  }
+});
+
+module.exports = fs;
