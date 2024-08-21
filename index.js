7f87c15ca5204c2a3a1fd602fba1d4b82683b5dd
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-21 10:31:54 +0545

    handle page routes in node js

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const fs = require("fs");
+
+const createServer = http.createServer((req, res) => {
+  const log = `${Date.now()}: ${req.url}/ New data Received \n`;
+  fs.appendFile("stats.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home Page");
+        break;
+      case "/about":
+        res.end("This is Projan1111");
+        break;
+      default:
+        res.end("404 Page not found");
+    }
+  });
+});
+
+createServer.listen(8000, (req, res) => {
+  console.log("server started! at port: http://localhost:8000");
+});
