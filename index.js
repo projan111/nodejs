1ee1021a3cfa463a102b667d2778c9ca9d420c44
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-21 17:20:53 +0545

    add search query

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const createServer = http.createServer((req, res) => {
+  // Disabling the favicon
   if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url}/ New data Received \n`;
 
+  // using url package targeting the specific value from the req.url
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+
+  //creating a log file and send all of the logs and store the local dir.
   fs.appendFile("stats.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("Home Page");
         break;
       case "/about":
-        res.end("This is Projan1111");
+        const username = myUrl.query.name;
+        res.end(`Hello ${username}`);
         break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end("Here is your search result " + search);
       default:
         res.end("404 Page not found");
     }
