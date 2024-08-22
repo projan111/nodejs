51cbc76409d1e8b53a597ba57458eb23928ca0c5
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-22 11:46:16 +0545

    add and delete un need files and dir

--- index.js
+++ index.js
@@ -2,34 +2,49 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-const createServer = http.createServer((req, res) => {
-  // Disabling the favicon
-  if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}:${req.method} ${req.url}/ New data Received \n`;
+const express = require("express");
+const app = express();
 
-  // using url package targeting the specific value from the req.url
-  const myUrl = url.parse(req.url, true);
-  console.log(myUrl);
-
-  //creating a log file and send all of the logs and store the local dir.
-  fs.appendFile("stats.txt", log, (err, data) => {
-    switch (myUrl.pathname) {
-      case "/":
-        res.end("Home Page");
-        break;
-      case "/about":
-        const username = myUrl.query.name;
-        res.end(`Hello ${username}`);
-        break;
-      case "/search":
-        const search = myUrl.query.search_query;
-        res.end("Here is your search result " + search);
-      default:
-        res.end("404 Page not found");
-    }
-  });
+app.get("/", (req, res) => {
+  return res.send("hello world");
 });
 
-createServer.listen(8000, (req, res) => {
+app.get("/about", (req, res) => {
+  return res.send(
+    "Hello " + req.query.name + " You are " + req.query.age + " years old."
+  );
+});
+
+const myServer = http.createServer(app);
+
+myServer.listen(8000, (req, res) => {
   console.log("server started! at port: http://localhost:8000");
 });
+
+// const createServer = http.createServer((req, res) => {
+//   // Disabling the favicon
+//   if (req.url === "/favicon.ico") return res.end();
+//   const log = `${Date.now()}:${req.method} ${req.url}/ New data Received \n`;
+
+//   // using url package targeting the specific value from the req.url
+//   const myUrl = url.parse(req.url, true);
+//   console.log(myUrl);
+
+//   //creating a log file and send all of the logs and store the local dir.
+//   fs.appendFile("stats.txt", log, (err, data) => {
+//     switch (myUrl.pathname) {
+//       case "/":
+//         res.end("Home Page");
+//         break;
+//       case "/about":
+//         const username = myUrl.query.name;
+//         res.end(`Hello ${username}`);
+//         break;
+//       case "/search":
+//         const search = myUrl.query.search_query;
+//         res.end("Here is your search result " + search);
+//       default:
+//         res.end("404 Page not found");
+//     }
+//   });
+// });
